416ca085a205ab63e5aa373c670f4fa6d6dd0301
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-08-29 16:01:54 +0800

    update footer

--- src/componets/Layout/FooterStyles.js
+++ src/componets/Layout/FooterStyles.js
@@ -7,13 +7,13 @@ const FooterStyles = styled.main.attrs({})`
         ${tw`px-24 py-10 bg-verydarkviolet text-center text-4xl text-brokenwhite flex-col lg:(grid justify-items-start flex flex-row justify-between text-xl)`}
     }
     #container-1 {
-      ${tw`font-black text-5xl pr-10`}
+      ${tw`font-black text-5xl pr-10 lg:(pt-5)`}
     } 
     #container-2 {
-      ${tw `flex flex-col lg:flex-row`}
+      ${tw `flex flex-col lg:(flex-row pt-5) `}
     }  
     #sub-footer {
-        ${tw`pr-10`}
+        ${tw`pr-10 pt-20 lg:(pt-0)`}
     }
     #sub-footer h1{
         ${tw`text-brokenwhite pb-3`}
@@ -22,7 +22,7 @@ const FooterStyles = styled.main.attrs({})`
         ${tw`text-gray p-2`}
     }
     #media-acoounts{
-      ${tw`flex justify-center`}
+      ${tw`flex justify-center pt-10 lg:(pt-5)`}
     }
     #media-acoounts #icon{
       ${tw`pr-5`}
