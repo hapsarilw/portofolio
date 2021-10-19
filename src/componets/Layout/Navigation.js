70131f2eae52f84f8d531201d255ae024799016f
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-10-19 16:59:49 +0800

    change use state module

--- src/componets/Layout/Navigation.js
+++ src/componets/Layout/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import NavigationStyles from "./NavigationStyles";
 // import { Transition } from "@headlessui/react";
 
